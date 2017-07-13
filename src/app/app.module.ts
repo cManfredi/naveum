badd72afaad05776cf74df0a083a8b6a57620634
cManfredi <c.manfredi.28@gmail.com>
2017-07-13 22:46:07 +0200

    Generazione componenti e sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
 import 'hammerjs';
+import {MdSidenavModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MdSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
