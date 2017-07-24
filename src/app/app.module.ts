fb942da153cc09c1acbe9a272ed7a604f8c8ad1b
cManfredi <c.manfredi.28@gmail.com>
2017-07-24 12:09:57 +0200

    Modifiche sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import 'hammerjs';
@@ -8,15 +9,25 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { PageNotFoundComponent } from './pageNotFound.component';
+
+/* Routes predefinite dell'applicazione, a mano a mano che si scoprono nuove pagine è possibile
+   ne verranno aggiunte altre alla configurazione del router */
+const appRoutes: Routes = [
+  { path: '', component: MainComponent},
+  { path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    PageNotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes, {enableTracing: true}),
     BrowserModule,
     BrowserAnimationsModule,
     MdSidenavModule,
