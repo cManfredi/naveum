db4dfea5c9a03d493cb8c7e1b8d6876b6c5204b3
cManfredi <c.manfredi.28@gmail.com>
2018-02-04 17:16:40 +0100

    aaa

--- src/app/info-room/info-room.component.ts
+++ src/app/info-room/info-room.component.ts
@@ -17,6 +17,8 @@ export class InfoRoomComponent implements OnInit {
   private svg: string;
   public artworks: Array<any>;
   selectedIndex: number;
+  currentArtwork: number;
+  inRoom: boolean;
 
   // tslint:disable-next-line:max-line-length
   constructor(private _router: Router, private _route: ActivatedRoute, private _globalService: GlobalService, private _jsonService: JsonFetchService) { }
@@ -24,6 +26,8 @@ export class InfoRoomComponent implements OnInit {
   ngOnInit() {
     this.artworks = [];
     this.selectedIndex = 0;
+    this.currentArtwork = 0;
+    this.inRoom = false;
     /* Sottoscrizione agli eventi che arrivano dal servizio condiviso */
     // this._sharedService.beaconLoad$.subscribe(bData => this.refresh(bData));
     // const initData: any = this._globalService.currentBeacon;
@@ -35,19 +39,27 @@ export class InfoRoomComponent implements OnInit {
         // Recupero l'id dell'opera da mostrare
         const id: number = Number.parseInt(paramMap.get('id'));
         const data: any = this._globalService.findRoom(id);
-        this.refresh(data);
+        this.refresh(id, data);
       };
     });
   }
 
-  refresh(bData: any) {
+  refresh(id, bData: any) {
     this.name = bData.title;
     this.svg = bData.imgUrl;
     this.description = bData.description;
+    this.currentArtwork = 0;
     bData.artworks.forEach(element => {
       const artwork = this._globalService.findArtwork(element);
       this.artworks.push({id: artwork.id, title: artwork.title});
+      if (this._globalService.linkToCurrent === 'artwork/' + artwork.id) {
+        this.currentArtwork = artwork.id;
+      }
     });
+    this.inRoom = false;
+    if (this._globalService.linkToCurrent === 'room/' + id) {
+      this.inRoom = true;
+    }
     this.artworks.sort((a, b) => a.id - b.id);
     this.selectedIndex = this._globalService.lastTabSelected;
   }
